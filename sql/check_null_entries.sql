82222de848b77096f795c5dbf351e6ebbfa6429d
kennyjin <jianianjin@gmail.com>
2020-12-01 20:16:04 -0500

    SQL data cleaning

---
+++ sql/check_null_entries.sql
@@ -0,0 +1,21 @@
+
+SELECT *
+FROM Table
+WHERE tripduration = 'NULL'
+	OR starttime = 'NULL'
+	OR stoptime = 'NULL'
+	OR `start station id` = 'NULL'
+	OR `start station name` = 'NULL'
+	OR `start station latitude` = 'NULL'
+	OR `start station longitude` = 'NULL'
+	OR `end station id` = 'NULL'
+	OR `end station name` = 'NULL'
+	OR `end station latitude` = 'NULL'
+	OR `end station longitude` = 'NULL'
+	OR `bikeid` = 'NULL'
+	OR `usertype` = 'NULL'
+	OR `birth year` = 'NULL'
+	OR `gender` = 'NULL';
+
+
+
